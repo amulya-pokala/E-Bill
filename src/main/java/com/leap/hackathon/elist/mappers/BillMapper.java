6073d9a44c00f5fe7af6016145ea708d6ca9b3dc
amulya-pokala <amulya22pokala@gmail.com>
2018-07-12 12:41:05 +0530

    added mapper and other stuff

---
+++ src/main/java/com/leap/hackathon/elist/mappers/BillMapper.java
@@ -0,0 +1,28 @@
+package com.leap.hackathon.elist.mappers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+
+import com.leap.hackathon.elist.models.Bill;
+
+
+
+
+public class BillMapper implements RowMapper<Bill>{
+
+	@Override
+	public Bill mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Bill bill = new Bill();
+		bill.setBillJson(rs.getString("bill_data"));
+		bill.setCustomerId(rs.getInt("customer_id"));
+		bill.setVendorId(rs.getInt("vendor_id"));
+		bill.setDateOfPurchase(rs.getDate("date"));
+		return bill;
+	}
+	
+	
+
+}
